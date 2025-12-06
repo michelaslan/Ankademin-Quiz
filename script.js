31d62c0ecc9d9403dff71662379ff3cb63f17661
Michel Aslan <michelaslan@live.se>
2025-12-06 19:12:12 +0100

    Added multichoise question

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const questions = [
 {
-    type: "truefalse",
-    question: "Nackademins frontendutbildning innehåller HTML, CSS och JavaScript.",
+    type: "trueFalse",
+    question: "Ankademins frontendutbildning innehåller HTML, CSS och JavaScript.",
     correctText: "Sant",
     correctAnswer: true
 },
+{
+    type: "multiChoise",
+    question: 'Vilken datatyp är ["äpple", "päron", "banan"] i JavaScript?',
+    correctText: "Array",
+    correctAnswer: true
+},
+
+
+
+
+/*{
+    type: "truefalse",
+    question: "Ankademins frontendutbildning är 3 år lång",
+    correctText: "Falskt",
+    correctAnswer: false
+},*/
 
 ];
 
@@ -12,8 +28,9 @@ const userAnswers = [];
 
 let counter = 0;
 let container = document.querySelector(".question-container");
+showQuestion();
 
-function showQuestion () {
+function showTrueFalse () {
     container.innerHTML = `
         <h2>Fråga ${counter + 1}</h2>
         <h3> ${questions[counter].question} </h3>
@@ -27,27 +44,63 @@ function showQuestion () {
         </label>
     `;
     }
-    
-showQuestion (counter);
-document.querySelector("#nextBtn").addEventListener("click", () => {
-    const selected = document.querySelector('input[name="svar"]:checked');
 
-    if (selected) {
-        userAnswers[counter] = selected.value === "true";
+function showQuestion (){
+    if (questions[counter].type === "trueFalse"){
+    showTrueFalse (counter);
     }
-    else {
-        userAnswers[counter] = null;
+    else if (questions[counter].type === "multiChoise") {
+        showMultiChoise (counter);
     }
-    
-    counter++;
+}
+function showMultiChoise () {
+    container.innerHTML = `
+        <h2>Fråga ${counter + 1}</h2>
+        <h3> ${questions[counter].question} </h3>
 
-    if (counter < questions.length) {
-        showQuestion(counter)
-    }
-    else {
-        container.innerHTML = "Du har svarat på alla frågor.";
-    }
+        <label>
+            <input type="radio" name="multiSvar" value="false"> Objekt
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="multiSvar" value="false"> Sträng
+        </label>
+        <label>
+            <input type="radio" name="multiSvar" value="true"> Array
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="multiSvar" value="false"> Nummer
+        </label>
+    `;
+}
+
+function nextButton () {
+    document.querySelector("#nextBtn").addEventListener("click", () => {
+    
+        let selected;
+
+        if (questions[counter].type === "trueFalse") {
+            selected = document.querySelector('input[name="svar"]:checked'); 
+        }
+        else if (questions[counter].type === "multiChoise") {
+            selected = document.querySelector('input[name="multiSvar"]:checked');
+        }
+        userAnswers[counter] = selected ? selected.value === "true": null;
+
+        counter++;
+
+        if (counter < questions.length) {
+            showQuestion(counter)
+        }
+        else {
+            container.innerHTML = "Du har svarat på alla frågor.";
+        }
 });
+}
+
+nextButton ();
+
 
 document.querySelector("#checkBtn").addEventListener("click", () => {
     container.innerHTML = "";
